4aae5121f3aee8d27f631e277024c53cf417a0dd
valmy <valmyericles@gmail.com>
2020-12-16 08:54:36 -0300

    redux store configure

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,14 +5,22 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 
 import '../styles/globals.css'
 
+import { Provider } from 'react-redux'
+import { PersistGate } from 'redux-persist/integration/react'
+import { store, persistor } from '../store'
+
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
   return (
     <>
-      <Head>
-        <title>OneBitGames</title>
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-      </Head>
-      <Component {...pageProps } />
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+          <Head>
+            <title>OneBitGames</title>
+            <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+          </Head>
+          <Component {...pageProps } />
+        </PersistGate>
+      </Provider>
     </>
   )
 }
