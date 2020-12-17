245c81d589abd1e965d9966698468e2c7d6140bd
valmy <valmyericles@gmail.com>
2020-12-16 23:41:14 -0300

    menu footer and lateral menu

---
+++ components/shared/AdminComponent/index.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Col, Row } from 'react-bootstrap'
+import AdminHeader from '../Header/Admin'
+import AdminFooter from '../Footer/AdminFooter'
+import LateralMenu from '../LateralMenu'
+
+const AdminComponent: React.FC = ({ children }) => {
+  return (
+    <Row className="mr-lg-4">
+      <Col lg={3}>
+        <LateralMenu/>
+      </Col>
+
+      <Col lg={9}>
+        <div className="d-flex flex-column sticky-footer-wrapper container">
+          <AdminHeader name="Valmy Ericles" />
+
+          <div className="flex-fill text-center">
+            {children}
+          </div>
+
+          <AdminFooter />
+        </div>
+      </Col>
+    </Row>
+  )
+}
+
+export default AdminComponent;
--- pages/Admin/index.tsx
+++ pages/Admin/index.tsx
@@ -1,8 +1,13 @@
 import React from 'react'
 import withAuthAdmin from '../../components/withAuthAdmin'
+import AdminComponent from '../../components/shared/AdminComponent'
 
 const Home: React.FC = () => {
-  return <h1>Você acessou o painel!</h1>
+  return (
+    <AdminComponent>
+      <h1>Você acessou o painel!</h1>
+    </AdminComponent>
+  ) 
 }
 
 export default withAuthAdmin(Home)
